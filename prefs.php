4ad374e6af7e68b9e9deac760bbcf825ced47865
BlackScorp <vitalij@blackscorp.de>
2016-07-20 21:05:32 +0200

    replaced split with preg_split

--- prefs.php
+++ prefs.php
@@ -230,7 +230,7 @@ if ($op=="suicide" && Settings::getsetting("selfdelete",0)!=0) {
 			}
 			
 			if(is_array($x[0])) $x[0] = call_user_func_array('sprintf', $x[0]);
-			$type = split(",", $x[0]);
+			$type = preg_split("/,/", $x[0]);
 			if (isset($type[1])) $type = trim($type[1]);
 			else $type = "string";
 
